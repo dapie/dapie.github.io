831cafcb7c03b1a68e9d3a8dbab78e6449e0baf8
dapie <napie.dapie@gmail.com>
2017-07-17 11:04:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".text").click(function() {
+        $(".center").animate({opacity: 0, marginBottom: "25%"}, 600, function(){
+            $(".text").css({display: "none"});
+            $(".center").css({marginBottom: "-25%"});
+            $(".links").css({display: "initial" , opacity: 1});
+        });
+        $(".center").animate({opacity: 1, marginBottom: "0"}, 600);
+    });
+    $(".links").click(function() {
+        $(".center").animate({opacity: 0, marginBottom: "25%"}, 600, function(){
+            $(".links").css({display: "none"});
+            $(".center").css({marginBottom: "-25%"});
+            $(".text").css({display: "initial" , opacity: 1});
+        });
+        $(".center").animate({opacity: 1, marginBottom: "0"}, 600);
+    });
+});
