39fcc3ff7311c49a37fc78370a3f008de955661d
dapie <napie.dapie@gmail.com>
2017-07-18 04:11:26 +0500

    Add files via upload

---
+++ sakura-team/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".text").click(function() {
+        $(".center").animate({opacity: 0, marginBottom: "25%"}, 600, function(){
+            $(".text").css({display: "none"});
+            $(".center").css({marginBottom: "-25%"});
+            $(".links").css({display: "initial" , opacity: 1});
+        });
+        $(".center").animate({opacity: 1, marginBottom: "0"}, 600);
+    });
+    $(".links").click(function() {
+        $(".center").animate({opacity: 0, marginBottom: "25%"}, 600, function(){
+            $(".links").css({display: "none"});
+            $(".center").css({marginBottom: "-25%"});
+            $(".text").css({display: "initial" , opacity: 1});
+        });
+        $(".center").animate({opacity: 1, marginBottom: "0"}, 600);
+    });
+    $(".nav li").click(function() {
+        $(".nav li.active").removeClass("active");
+        $(this).addClass("active");
+    });
+});
