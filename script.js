b1f3a5b176efb837691a7d5bc09255f65a7fff29
dapie <napie.dapie@gmail.com>
2017-07-17 11:44:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
-    $(".text").click(function() {
-        $(".center").animate({opacity: 0, marginBottom: "25%"}, 600, function(){
+        $(".center").delay(3000).animate({opacity: 0, marginBottom: "25%"}, 600, function(){
             $(".text").css({display: "none"});
             $(".center").css({marginBottom: "-25%"});
             $(".links").css({display: "initial" , opacity: 1});
         });
         $(".center").animate({opacity: 1, marginBottom: "0"}, 600);
-    });
     $(".links").click(function() {
         $(".center").animate({opacity: 0, marginBottom: "25%"}, 600, function(){
             $(".links").css({display: "none"});
