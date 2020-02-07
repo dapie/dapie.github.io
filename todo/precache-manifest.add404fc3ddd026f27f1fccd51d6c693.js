b9ecee7be6f4ab166132d1406943546b558fa745
Denis Tkachev <napie.dapie@gmail.com>
2020-02-07 13:10:30 +0500

    Add todo files (React)

---
+++ todo/precache-manifest.add404fc3ddd026f27f1fccd51d6c693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdabe70ceef4b806f93f22c47fdc11ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fb3365791111d081038",
+    "url": "/static/css/main.eb1cf347.chunk.css"
+  },
+  {
+    "revision": "fae43b654ed64f39da5b",
+    "url": "/static/js/2.d99243ac.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d99243ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb3365791111d081038",
+    "url": "/static/js/main.5e33ac31.chunk.js"
+  },
+  {
+    "revision": "452efe56c2036ff11208",
+    "url": "/static/js/runtime-main.83c66be5.js"
+  }
+]);
