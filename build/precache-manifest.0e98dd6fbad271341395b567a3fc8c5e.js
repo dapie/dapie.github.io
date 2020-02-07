df5a48bd2bb8319df930fd9e66378f1e6dea68bd
Denis Tkachev <napie.dapie@gmail.com>
2020-02-07 13:23:33 +0500

    Rebuild react app

---
+++ build/precache-manifest.0e98dd6fbad271341395b567a3fc8c5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8665dbbefe63f2bb1e9a0d63518a6b39",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "d5e2d5614756e1e0bd2c",
+    "url": "/todo/static/css/main.eb1cf347.chunk.css"
+  },
+  {
+    "revision": "fae43b654ed64f39da5b",
+    "url": "/todo/static/js/2.d99243ac.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo/static/js/2.d99243ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5e2d5614756e1e0bd2c",
+    "url": "/todo/static/js/main.9d0c9006.chunk.js"
+  },
+  {
+    "revision": "dd9adfdb09f83cb1a7aa",
+    "url": "/todo/static/js/runtime-main.f7a45fe9.js"
+  }
+]);
