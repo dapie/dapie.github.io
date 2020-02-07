d21620eb2231cd4fea3e34c7512dc3abf4a88220
Denis Tkachev <napie.dapie@gmail.com>
2020-02-07 13:43:38 +0500

    Add files via upload

--- todo/service-worker.js
+++ todo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.add404fc3ddd026f27f1fccd51d6c693.js"
+  "/todo/precache-manifest.0e98dd6fbad271341395b567a3fc8c5e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
